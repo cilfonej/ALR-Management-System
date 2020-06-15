b2874b3ad136a59ecd31db63206d0762bf2f6936
Andy Ngo <ngoa2@wit.edu>
2020-06-15 17:48:37 -0400

    Started work on web controller

---
+++ Dog Days/src/main/java/edu/wit/alr/services/TransportService.java
@@ -0,0 +1,31 @@
+package edu.wit.alr.services;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.wit.alr.database.model.Address;
+import edu.wit.alr.database.model.Dog;
+import edu.wit.alr.database.model.TransportReservation;
+import edu.wit.alr.database.model.roles.Caretaker;
+import edu.wit.alr.database.repository.TransportReservationRepository;
+
+
+@Service
+public class TransportService {
+	
+	@Autowired
+	private TransportReservationRepository repository;
+	
+	public void createTransport(Address address, Dog dog, Caretaker person, LocalDate pickupDate) {
+		TransportReservation transport = new TransportReservation(dog); 
+		transport.setPickupAddress(address);
+		transport.setPickupPerson(person);
+		transport.setTransportDate(pickupDate);
+		
+		//TODO SEND EMAIL TO PEOPLE ABOPUT THIS
+		repository.save(transport);
+	}
+	
+}
