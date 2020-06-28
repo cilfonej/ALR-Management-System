9c0654a48a8ff00fd8e70868ce354c5846ae87e7
Joshua Cilfone <cilfonej@wit.edu>
2020-06-28 06:27:45 -0400

    Started work on layout of "View-Dog" page

--- Dog Days/src/main/java/edu/wit/alr/web/controllers/pages/ViewDogController.java
+++ Dog Days/src/main/java/edu/wit/alr/web/controllers/pages/ViewDogController.java
@@ -5,8 +5,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.wit.alr.database.model.Dog;
@@ -23,7 +23,13 @@ public class ViewDogController {
 	@Autowired private DogService dogService;
 	
 	@GetMapping("")
-	protected @ResponseBody String loadPage_direct(@RequestParam("id") int id) {
+	protected @ResponseBody String list_direct() {
+		return builder.buildIndependentPage(listPage());
+	}
+	
+	@GetMapping("/{id}")
+	protected @ResponseBody String loadPage_direct(@PathVariable("id") Integer id) {
+		if(id == null) return list_direct();
 		return builder.buildIndependentPage(loadPage(id));
 	}
 	
@@ -33,6 +39,10 @@ public class ViewDogController {
 	
 	public @ResponseBody PageResponse loadPage(Dog dog) {
 		if(dog == null) ; // TODO: build error page
-		return builder.redirect("/view/dogs?id=" + dog.getID(), "pages/dog/view/view_dog :: page", Map.of("dog", dog));
+		return builder.redirect("/view/dogs/" + dog.getID(), "pages/dog/view/view_dog :: page", Map.of("dog", dog));
+	}
+	
+	public @ResponseBody PageResponse listPage() {
+		return builder.redirect("/view/dogs", "pages/dog/list/list_dogs :: page", null);
 	}
 }
