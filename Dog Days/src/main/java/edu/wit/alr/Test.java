2e3c27188d4dcee2b7c5a07020b426a63d74c6a8
Joshua Cilfone <cilfonej@wit.edu>
2020-07-13 14:30:41 -0400

    Changes application.properties to *.yaml and added secret.properties

--- Dog Days/src/main/java/edu/wit/alr/Test.java
+++ Dog Days/src/main/java/edu/wit/alr/Test.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@PropertySource("classpath:secret.yaml")
 public class Test {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Test.class, args);
