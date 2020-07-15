b6703a994872475dc27f1a32c258616034320151
Joshua Cilfone <cilfonej@wit.edu>
2020-07-15 07:50:28 -0400

    Started switch to Redis based session storage

---
+++ Dog Days/src/main/java/edu/wit/alr/web/session/SessionInitializer.java
@@ -0,0 +1,7 @@
+package edu.wit.alr.web.session;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
+
+}
