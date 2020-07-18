dabb42cb611cf76984708f2a12d5ae1b2c5d88e2
Joshua Cilfone <cilfonej@wit.edu>
2020-07-18 13:36:54 -0400

    Switched "secret.properties" to *.yaml

--- Dog Days/src/main/java/edu/wit/alr/Test.java
+++ Dog Days/src/main/java/edu/wit/alr/Test.java
@@ -8,9 +8,11 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
+import edu.wit.alr.config.YamlPropertyLoaderFactory;
+
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @EnableConfigurationProperties
-@PropertySource("classpath:/secret.properties")
+@PropertySource(value = "classpath:/secret.yaml", factory = YamlPropertyLoaderFactory.class)
 public class Test {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Test.class, args);
